5acd0326b4834912de57bd58044998c2c7febde9
OtaraAlex <mbechealex61@gmail.com>
2023-08-11 14:47:04 +0300

    update redirect after logging out

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -35,18 +35,18 @@ function LeftSidebar() {
             )}
             </div>
 
-            <div className="mt-10 px-6">
+            <div className='mt-10 px-6'>
                 <SignedIn>
-                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                    <SignOutButton signOutCallback={() => router.push("/sign-in")}>
                         <div className='flex cursor-pointer gap-4 p-4'>
-                            <Image 
-                                src='/assets/logout.svg' 
-                                alt='logout' 
-                                width={24} 
+                            <Image
+                                src='/assets/logout.svg'
+                                alt='logout'
+                                width={24}
                                 height={24}
                             />
 
-                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                            <p className='text-light-2 max-lg:hidden'>Logout</p>
                         </div>
                     </SignOutButton>
                 </SignedIn>
